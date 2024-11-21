822265e730334934102263de4aed5c5913989860
Ryan <anhoang800140@gmail.com>
2024-11-22 01:42:07 +0700

    Adding send email through emailJS

--- Javascript/index.js
+++ Javascript/index.js
@@ -134,3 +134,26 @@ themeSwitch.addEventListener("click", () => {
   darkmode = localStorage.getItem("darkmode");
   darkmode !== "active" ? enableDarkmode() : disableDarkmode();
 });
+const btn = document.getElementById("sendBtn");
+
+document
+  .getElementById("contactForm")
+  .addEventListener("submit", function (event) {
+    event.preventDefault();
+
+    btn.value = "Sending...";
+
+    const serviceID = "default_service";
+    const templateID = "template_71brhqg";
+
+    emailjs.sendForm(serviceID, templateID, this).then(
+      () => {
+        btn.value = "Send Email";
+        alert("Sent successfully!");
+      },
+      (err) => {
+        btn.value = "Send Email";
+        alert(JSON.stringify(err));
+      }
+    );
+  });
